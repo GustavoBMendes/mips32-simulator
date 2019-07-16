bc311d6d28fa677d0f81c38251030eeae66eb4e0
fersilverio <ssilveriofernando45@gmail.com>
2019-07-16 15:26:41 -0300

    Inicio dos registradores

---
+++ includes/registradores.h
@@ -0,0 +1,39 @@
+#ifndef RESGISTERS_H
+
+//Mudar para o tipo adequado para o vetor
+extern unsigned int reg[32];
+
+#define T1 reg[0]
+#define T2 reg[1]
+#define T3 reg[2]
+#define T4 reg[3]
+#define T5 reg[4]
+#define T6 reg[5]
+#define T7 reg[6]
+#define T8 reg[7]
+#define T9 reg[8]
+#define T10 reg[9]
+#define T11 reg[10]
+#define T12 reg[11]
+#define T13 reg[12]
+#define T14 reg[13]
+#define T15 reg[14]
+#define T16 reg[15]
+#define T17 reg[16]
+#define T18 reg[17]
+#define T19 reg[18]
+#define T20 reg[19]
+#define T21 reg[20]
+#define T22 reg[21]
+#define T23 reg[22]
+#define T24 reg[23]
+#define T25 reg[24]
+#define T26 reg[25]
+#define T27 reg[26]
+#define T28 reg[27]
+#define T29 reg[28]
+#define T30 reg[29]
+#define T31 reg[30]
+#define T32 reg[31]
+
+#endif
