b7552fab3cf81d3457601208fed797322437cbee
fersilverio <ssilveriofernando45@gmail.com>
2019-07-17 15:06:42 -0300

    Começando as instruções

---
+++ includes/instrucoes.h
@@ -0,0 +1,42 @@
+#ifndef INSTRUCTIONS_H
+#define INSTRUCTIONS_H
+
+#include "registradores.h"
+//Falta definir os registradores que iriam entrar nas operações porém os desconheço
+//Acredito que com certeza não serão do tipo void mas isso vamos ver ainda, por enquanto meti um void
+void add(int regDestino,int regOrigem1, int regOrigem2);
+void addi();
+void And(int regDestino,int regOrigem1, int regOrigem2);
+void andi();
+void b();
+void beq();
+void beql();
+void bgez();
+void bgtz();
+void blez();
+void bltz();
+void bne();
+void div();
+void j();
+void jr();
+void lui();
+void madd();
+void mfhi();
+void mflo();
+void movn(int regDestino,int regOrigem1, int regOrigem2);
+void movz(int regDestino,int regOrigem1, int regOrigem2);
+void msub();
+void mthi();
+void mtlo();
+void mul(int regDestino,int regOrigem1, int regOrigem2);
+void mult();
+void nop();
+void nor(int regDestino,int regOrigem1, int regOrigem2);
+void Or(int regDestino,int regOrigem1, int regOrigem2);
+void ori();
+void sub(int regDestino,int regOrigem1, int regOrigem2);
+void Xor(int regDestino,int regOrigem1, int regOrigem2);
+void xori();
+
+
+#endif
---
+++ instrucoes.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "includes/instrucoes.h"
+
+//Falta definir os registradores que iriam entrar nas operações porém os desconheço
+//Acredito que com certeza não serão do tipo void mas isso vamos ver ainda, por enquanto meti um void
+
+void add(){
+
+}
+
+void addi(){
+
+}
+
+void op_and(){
+
+}
+
+void andi(){
+
+}
+
+void b(){
+
+}
+
+void beq(){
+
+}
+
+void beql(){
+
+}
+
+void bgez(){
+
+}
+
+void bgtz(){
+
+}
+
+void blez(){
+
+}
+
+void bltz(){
+
+}
+
+void bne(){
+
+}
+
+void div(){
+
+}
+
+void j(){
+
+}
+
+void jr(){
+
+}
+
+void lui(){
+
+}
+
+void madd(){
+
+}
+
+void mfhi(){
+
+}
+
+void mflo(){
+
+}
+
+void movn(){
+
+}
+
+void movz(){
+
+}
+
+void msub(){
+
+}
+
+void mthi(){
+
+}
+
+void mtlo(){
+
+}
+
+void mul(){
+
+}
+
+void mult(){
+
+}
+
+void nop(){
+
+}
+
+void nor(){
+
+}
+
+void op_or(){
+
+}
+
+void ori(){
+
+}
+
+void sub(){
+
+}
+
+void op_xor(){
+
+}
+
+void xori(){
+
+}
