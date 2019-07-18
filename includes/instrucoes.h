9d86505b5a5af6fe86cf7fc044b97a65da875b55
GustavoBMendes <mendes.gustavob@gmail.com>
2019-07-18 16:11:52 -0300

    ula/instructions

--- includes/instrucoes.h
+++ includes/instrucoes.h
@@ -7,9 +7,9 @@ extern unsigned int imediato;
 
 
 //Acredito que com certeza não serão do tipo void mas isso vamos ver ainda, por enquanto meti um void
-void add(int regDestino,int regOrigem1, int regOrigem2);
+int add(int regDestino,int regOrigem1, int regOrigem2);
 void addi(int regDestino,int regOrigem1, int imediato);
-void And(int regDestino,int regOrigem1, int regOrigem2);
+int And(int regDestino,int regOrigem1, int regOrigem2);
 void andi(int regDestino,int regOrigem1, int imediato);
 void b(int PC, int offset);
 void beq(int regOrigem1,int regOrigem2,int PC, int offset);
@@ -35,10 +35,10 @@ void mul(int regDestino,int regOrigem1, int regOrigem2);
 void mult(int regOrigem1, int regOrigem2, int regAcumulador);
 void nop();
 void nor(int regDestino,int regOrigem1, int regOrigem2);
-void Or(int regDestino,int regOrigem1, int regOrigem2);
+int Or(int regDestino,int regOrigem1, int regOrigem2);
 void Ori(int regDestino,int regOrigem1, int imediato);
-void sub(int regDestino,int regOrigem1, int regOrigem2);
-void Xor(int regDestino,int regOrigem1, int regOrigem2);
+int sub(int regDestino,int regOrigem1, int regOrigem2);
+int Xor(int regDestino,int regOrigem1, int regOrigem2);
 void Xori(int regDestino,int regOrigem1, int imediato);
 
 
