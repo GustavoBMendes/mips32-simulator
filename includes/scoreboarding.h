fe2449d631da84662f45d2eb032af391365fd880
fersilverio <ssilveriofernando45@gmail.com>
2019-08-06 15:11:51 -0300

    Começando a provocar a besta enjaulada

---
+++ includes/scoreboarding.h
@@ -0,0 +1,31 @@
+#ifndef SCORE_H
+#define SCORE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+//Retornará o número de ciclos total levado para o scoreboarding do codigo analisado
+int cicloClock;
+//Num de instruções que o .asm possui
+int numInstrucoes;
+//Essa matriz devolverá a ordem de execução das instruções
+int instructionStatus[numInstrucoes][4];
+
+typedef struct functionalUnitStatus{
+    char *nomeUnidade,*opName,*fi,*fj,*fk,*qj,*qk;
+    bool busy,rj,rk;
+};
+
+functionalUnitStatus fus[5];
+
+typedef struct registerResultStatus{
+    char *regName;
+    char *ufName;
+}
+
+registerResultStatus rss[32];
+
+
+#endif
